a66b1a7b988119ed08acc05b6182c869a60473dd
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-24 06:30:52 +0200

    feat: add startup script

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-import { prepStart } from "./prepStart";
+import { prepStart } from "./prepStart.js";
 
-const { queues, consumers, producers, messages } = prepStart(1, 1, 2, 10);
+(async () => {
+  const { queues, consumers, producers, messages } = await prepStart(
+    1,
+    5,
+    1,
+    1
+  );
+  const times = {};
+  consumers.forEach(async (consumer) => {
+    await consumer.consumerObj.connect();
+    consumer.consumerObj.setOnMessage((messageString) => {
+      const message = JSON.parse(messageString.data);
+      const timeDiff = Date.now() - message.createdAt;
+      console.log(`time diff for message ${message.message} is ${timeDiff}`);
+    });
+  });
+  producers.forEach((prod) => {
+    messages.forEach((messageID) => {
+      const currMessage = { message: messageID, createdAt: Date.now() };
+      times[messageID] = currMessage;
+      prod.producerObj.publish(JSON.stringify(currMessage));
+    });
+  });
+})();
