349459515b09a04476b35d75ec337ecb5e72128b
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-24 04:52:06 +0200

    feat: add prepQueues script

---
+++ prepQueues.js
@@ -0,0 +1,18 @@
+import { uuid4 } from "uuid4";
+import { Queue } from "@mkamar/mq-lib";
+
+export const prepQueues = (queueNums) => {
+  let queues = [];
+  for (let i = 0; i < queueNums; i++) {
+    queues.push({
+      id: uuid4(),
+      createdAt: Date.now(),
+      queueObj: Queue.craeteQueue({
+        url: "localhost:3491",
+        queueKey: "tq1",
+        queueType: "direct",
+      }),
+    });
+    return queues;
+  }
+};
