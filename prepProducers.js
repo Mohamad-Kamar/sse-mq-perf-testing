b169241b0f88a01ddfb4d2bead870919627f1159
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-24 05:07:59 +0200

    feat: add preproducers script

---
+++ prepProducers.js
@@ -0,0 +1,17 @@
+import { uuid4 } from "uuid4";
+import { Producer } from "@mkamar/mq-lib";
+
+export const prepProducers = (consumerNums, queueKey) => {
+  let producers = [];
+  for (let i = 0; i < consumerNums; i++) {
+    const id = uuid4();
+    producers.push({
+      id,
+      createdAt: Date.now(),
+      consumerObj: new Producer("localhost:3491", {
+        queueKey,
+      }),
+    });
+    return producers;
+  }
+};
