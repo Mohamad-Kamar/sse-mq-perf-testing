eed3503c8c04c92519d3276e7d5d15b2c9517886
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-04-22 15:41:35 +0700

    Add implementation for sseMqAdapter

--- sseMQAdapter.js
+++ sseMQAdapter.js
@@ -1,26 +1,64 @@
+import { Consumer, Producer, Queue } from '@mkamar/mq-lib';
+
 class SSEMQAdapter {
   constructor(baseUrl) {
     this.baseUrl = baseUrl;
   }
 
-  async connect(/* ... */ ) {
-    // ... implementation ...
+  async connect(consumer) {
+    await consumer.consumerObj.connect();
+    return consumer;
   }
 
-  async createProducers(/* ... */ ) {
-    // ... implementation ...
+  async createProducers(producerNums, queueKey) {
+    let producers = [];
+    for (let i = 0; i < producerNums; i++) {
+      const id = uuidv4();
+      producers.push({
+        id,
+        createdAt: Date.now(),
+        producerObj: new Producer(this.baseUrl, {
+          queueKey,
+        }),
+      });
+    }
+    return producers;
   }
 
-  async createConsumers(/* ... */ ) {
-    // ... implementation ...
+  async createConsumers(consumerNums, queueKey) {
+    let consumers = [];
+    const createdAt = Date.now();
+    for (let i = 0; i < consumerNums; i++) {
+      const id = uuidv4();
+      consumers.push({
+        id,
+        createdAt,
+        consumerObj: new Consumer(this.baseUrl, {
+          queueKey,
+          consumerID: id,
+        }),
+        receivedTimes: [],
+      });
+    }
+    return consumers;
   }
 
-  async sendMessages(/* ... */ ) {
-    // ... implementation ...
+  async sendMessages(producer, messages) {
+    messages.forEach((messageID) => {
+      const currMessage = { message: messageID, createdAt: Date.now() };
+      producer.producerObj.publish(JSON.stringify(currMessage));
+    });
   }
 
-  async deleteQueue(/* ... */ ) {
-    // ... implementation ...
+  async deleteConsumer(consumer) {
+    consumer.consumerObj.delete();
+  }
+
+  async deleteQueue(queue) {
+    Queue.deleteQueue({
+      url: this.baseUrl,
+      queueKey: queue.id,
+    });
   }
 }
 
