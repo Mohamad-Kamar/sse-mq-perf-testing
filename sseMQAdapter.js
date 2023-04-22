e5feb7f36dede9af1e1567c78560c4c9026a7326
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-04-22 15:38:34 +0700

    Add SSE-MQ adapter for expanding to other sdks

---
+++ sseMQAdapter.js
@@ -0,0 +1,27 @@
+class SSEMQAdapter {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  async connect(/* ... */ ) {
+    // ... implementation ...
+  }
+
+  async createProducers(/* ... */ ) {
+    // ... implementation ...
+  }
+
+  async createConsumers(/* ... */ ) {
+    // ... implementation ...
+  }
+
+  async sendMessages(/* ... */ ) {
+    // ... implementation ...
+  }
+
+  async deleteQueue(/* ... */ ) {
+    // ... implementation ...
+  }
+}
+
+export default SSEMQAdapter;
