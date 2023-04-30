bd7d3f4aedc9bf8e74643a7a8a3d1fc1b2f4339a
mkamar <mohamad.kamar@agoda.com>
2023-04-30 13:58:35 +0700

    feat: simplify ssemq adapter main functinoality

--- messageQueueAdapters/sseMQAdapter.js
+++ messageQueueAdapters/sseMQAdapter.js
@@ -1,4 +1,4 @@
-import { Consumer, Producer } from '@mkamar/mq-lib';
+import { Consumer, Queue, Producer } from '@mkamar/mq-lib';
 import { v4 as uuidv4 } from 'uuid';
 
 class SSEMQAdapter {
@@ -14,53 +14,45 @@ class SSEMQAdapter {
     await consumerObj.connect();
   }
 
-  async createProducers(producerNums, queueKey) {
-    const producers = [];
-    const start = Date.now();
-    for (let i = 0; i < producerNums; i += 1) {
-      const id = uuidv4();
-      producers.push({
-        id,
-        createdAt: Date.now(),
-        producerObj: new Producer(this.baseUrl, {
-          queueKey,
-        }),
+  async createQueues(numOfQueues) {
+    const id = uuidv4();
+    const queues = [];
+    for (let i = 0; i < numOfQueues; i += 1) {
+      const currQ = Queue.craeteQueue({
+        url: 'http://localhost:3491',
+        queueKey: id,
+        queueType: 'fanout',
       });
+      queues.push(currQ);
     }
-    const end = Date.now();
-    const elapsedTime = end - start;
-    return { producers, elapsedTime };
+    return Promise.all(queues);
   }
 
-  async createConsumers(consumerNums, queueKey) {
+  async createProducers(queue, producerNums) {
+    const { queueKey } = queue;
+    const producers = [];
+    for (let i = 0; i < producerNums; i += 1) {
+      producers.push(new Producer(this.baseUrl, { queueKey }));
+    }
+    return producers;
+  }
+
+  async createConsumers(queue, consumerNums) {
+    const { queueKey } = queue;
     const consumers = [];
-    const createdAt = Date.now();
     for (let i = 0; i < consumerNums; i += 1) {
       const id = uuidv4();
-      consumers.push({
-        id,
-        createdAt,
-        consumerObj: new Consumer(this.baseUrl, {
-          queueKey,
-          consumerID: id,
-        }),
-        receivedTimes: [],
-      });
+      consumers.push(new Consumer(this.baseUrl, { queueKey, consumerID: id }));
     }
-    const end = Date.now();
-    const elapsedTime = end - createdAt;
-    return { consumers, elapsedTime };
+    await Promise.all(consumers.map((cons) => cons.createConsumer()));
+    return consumers;
   }
 
   async sendMessages(producer, messages) {
-    const start = Date.now();
-    messages.forEach((messageID) => {
+    return messages.map((messageID) => {
       const currMessage = { message: messageID, createdAt: Date.now() };
-      producer.producerObj.publish(JSON.stringify(currMessage));
+      return producer.producerObj.publish(JSON.stringify(currMessage));
     });
-    const end = Date.now();
-    const elapsedTime = end - start;
-    return { elapsedTime };
   }
 
   async deleteConsumer(consumer) {
