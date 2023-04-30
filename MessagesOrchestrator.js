254fe8ade08ef70e42468f62ae552a66d59cd847
mkamar <mohamad.kamar@agoda.com>
2023-04-30 17:57:20 +0700

    refactor: treat orchestator as singlton

--- MessagesOrchestrator.js
+++ MessagesOrchestrator.js
@@ -1,4 +1,4 @@
-export class MessagesOrchestrator {
+class MessagesOrchestrator {
   messages = {};
 
   addMessage(messageID) {
@@ -35,3 +35,5 @@ export class MessagesOrchestrator {
     return allElapsedTime.length / allElapsedTime;
   }
 }
+
+export const orchestrator = new MessagesOrchestrator();
--- config.js
+++ config.js
@@ -1,10 +1,8 @@
 import SSEMQAdapter from './messageQueueAdapters/sseMQAdapter';
 import RabbitMQAdapter from './messageQueueAdapters/rabbitMQAdapter';
 import KafkaAdapter from './messageQueueAdapters/kafkaAdapter';
-import { MessagesOrchestrator } from './MessagesOrchestrator';
 
 const MESSAGE_QUEUE = 'ssemq'; // Change this value to switch between different adapters.
-const messageOrc = new MessagesOrchestrator();
 
 const rabbitMQConfig = {
   url: 'amqp://localhost',
@@ -20,9 +18,9 @@ const sseMQConfig = {
 };
 
 const adapters = {
-  ssemq: new SSEMQAdapter(sseMQConfig.baseUrl, messageOrc),
-  rabbitmq: new RabbitMQAdapter(rabbitMQConfig.url, messageOrc),
-  kafka: new KafkaAdapter(kafkaConfig, messageOrc),
+  ssemq: new SSEMQAdapter(sseMQConfig.baseUrl),
+  rabbitmq: new RabbitMQAdapter(rabbitMQConfig.url),
+  kafka: new KafkaAdapter(kafkaConfig),
 };
 
 export const adapter = adapters[MESSAGE_QUEUE];
